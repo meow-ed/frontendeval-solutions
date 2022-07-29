fd7ccd877e5440c071748753bfe76a694a2e3e50
meowed <110143371+meow-ed@users.noreply.github.com>
2022-07-29 01:52:40 -0400

    add additional arguments to map function

--- array-prototype-map/main.js
+++ array-prototype-map/main.js
@@ -1,7 +1,7 @@
 function map(array, callback) {
     const result = []
     for (let i = 0; i < array.length; i++) {
-        result.push(callback(array[i]))
+        result.push(callback(array[i], i, array))
     }
     return result
 }
