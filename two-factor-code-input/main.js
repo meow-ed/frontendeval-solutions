0a08d5df6b79ef0c734e8f5e63e0d2d9f0a75b64
meowed <110143371+meow-ed@users.noreply.github.com>
2022-07-29 02:48:44 -0400

    add base files

---
+++ two-factor-code-input/main.js
@@ -0,0 +1,11 @@
+const correctCode = `8910`
+
+document.querySelector('.submit').addEventListener('click', handleSubmit)
+
+function handleSubmit(e) {
+    const code = ''
+    submitCode(code)
+}
+
+function submitCode(code) {
+}
